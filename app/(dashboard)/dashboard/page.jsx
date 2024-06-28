450814fb69991d1df5cd9148c28ae98f872a0300
fitmamul <itmamul2004@gmail.com>
2024-06-28 12:54:51 +0700

    fix

--- app/(dashboard)/dashboard/page.jsx
+++ app/(dashboard)/dashboard/page.jsx
@@ -243,8 +243,16 @@ const Dashboard = () => {
             {
                 label: '# of Votes',
                 data: [12, 19, 3, 5],
-                backgroundColor: ['#007200', '#9EF01A', '#70E000', '#004B23'],
-                borderColor: ['#007200', '#9EF01A', '#70E000', '#004B23'],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                ],
                 borderWidth: 1,
             },
         ],
@@ -256,9 +264,17 @@ const Dashboard = () => {
             {
                 label: '# of Votes',
                 data: [12, 19],
-                backgroundColor: ['#007200', '#9EF01A'],
-                borderColor: ['#007200', '#9EF01A'],
                 borderWidth: 1,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(54, 162, 235, 1)',
+                ],
             },
         ],
     }
@@ -366,17 +382,11 @@ const Dashboard = () => {
                     'rgba(255, 99, 132, 0.2)',
                     'rgba(54, 162, 235, 0.2)',
                     'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
                 ],
                 borderColor: [
                     'rgba(255, 99, 132, 1)',
                     'rgba(54, 162, 235, 1)',
                     'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
                 ],
                 borderWidth: 1,
             },
@@ -412,13 +422,6 @@ const Dashboard = () => {
             label: 'Cabang',
         },
     ]
-
-    const next = () => {
-        sliderRef.slickNext()
-    }
-    const previous = () => {
-        sliderRef.slickPrev()
-    }
 
     return (
         <div className="flex flex-col gap-5">
